7ebf8b39f7ce635db3c6df701062153776f2385c
Debora Tristao <48920353+debytristao@users.noreply.github.com>
2020-02-15 13:57:53 -0400

    Assingment2

---
+++ Quiz/app/src/main/java/work/nbcc/quiz/MainActivity.kt
@@ -0,0 +1,18 @@
+package work.nbcc.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
