6bd4d9aa37cbff4e396a3c12faf0bdacbd597292
Debora Tristao <48920353+debytristao@users.noreply.github.com>
2020-02-17 18:39:36 -0400

    Update

--- Quiz/app/src/main/java/work/nbcc/quiz/MainActivity.kt
+++ Quiz/app/src/main/java/work/nbcc/quiz/MainActivity.kt
@@ -6,12 +6,33 @@ import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.ActivityNavigator
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var layout: DrawerLayout
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityNavigator>(this,
+            R.layout.activity_main)
+
+        navController = this.findNavController(R.id.myNavHostFragment)
+        layout = binding.layout
+
+        NavigationUI.setupActionBarWithNavController(binding.navViewe, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, layout)
+
+
     }
 
 
--- Quiz/app/src/main/java/work/nbcc/quiz/cheat_Fragment.kt
+++ Quiz/app/src/main/java/work/nbcc/quiz/cheat_Fragment.kt
@@ -6,18 +6,72 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kotlinx.android.synthetic.main.fragment_cheat_.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class cheat_Fragment : Fragment() {
 
+
+    private val questionBank = listOf(
+        Question(R.string.question_1, false),
+        Question(R.string.question_2, true),
+        Question(R.string.question_3, true),
+        Question(R.string.question_4, false),
+        Question(R.string.question_5, false),
+        Question(R.string.question_6, true),
+        Question(R.string.question_7, false),
+        Question(R.string.question_8, true),
+        Question(R.string.question_9, false),
+        Question(R.string.question_10, false),
+        Question(R.string.question_11, false),
+        Question(R.string.question_12, true),
+        Question(R.string.question_13, false),
+        Question(R.string.question_14, true),
+        Question(R.string.question_15, false),
+        Question(R.string.question_16, false),
+        Question(R.string.question_17, true),
+        Question(R.string.question_18, false),
+        Question(R.string.question_19, false),
+        Question(R.string.question_20, true))
+
+    private lateinit var binding: cheat_Fragment
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var args : cheat_Fragment = cheat_Fragment().fromBundle(arguments!!)
+
+
+        binding.apply{
+
+            cheatLabel.setText(questionBank[args.question].textResId)
+
+            val answ: String = if (args.answer == true) "TRUE" else "FALSE"
+
+            answerLabel.visibility = View.INVISIBLE
+
+            yesButton.setOnClickListener{
+                answerLabel.visibility = View.VISIBLE
+            }
+
+            noButton.setOnClickListener{
+                activity!!.onBackPressed()
+            }
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cheat_, container, false)
+        //return inflater.inflate(R.layout.fragment_cheat_, container, false)
+        binding = DataBindingUtil.inflate( inflater, R.layout.fragment_cheat_, container, false)
+
+        return  binding.root
     }
 
 
